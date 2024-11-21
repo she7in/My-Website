ba8368a874b07e34731610a263b198aca5c39db4
Shehin Dileep <shehindileep@gmail.com>
2024-11-21 12:29:03 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [aspectRatio]
+};
